d4870e014449c31838128b3d9be183178bd5dcf3
Kushan Pabasara <kushan@trumpcode.com>
2018-09-08 23:51:33 +0530

    evote add and update fix

--- src/app/views/evote/evote-table/evote-table.component.ts
+++ src/app/views/evote/evote-table/evote-table.component.ts
@@ -14,6 +14,7 @@ import { AppConfirmService } from "../../../shared/services/app-confirm/app-conf
 import * as moment from "moment";
 import { AppFileDownloadService } from "../../../shared/services/file-download.service";
 import { AppDataConversionService } from '../../../shared/services/data-conversion.service';
+import { EvotePopupComponent } from '../../evote/evote-table/evote-popup/evote-popup.component';
 
 @Component({
   selector: 'app-evote-table',
@@ -32,11 +33,12 @@ export class EvoteTableComponent implements OnInit {
     private errDialog: AppErrorService,
     private confirmService: AppConfirmService,
     private downloadService: AppFileDownloadService,
-    private conversionService : AppDataConversionService
+    private conversionService : AppDataConversionService,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit() {
-    this.getAllProduct();
+    this.getAllEvote();
   }
 
   // downloadCsv(selectedRow) {
@@ -71,7 +73,7 @@ export class EvoteTableComponent implements OnInit {
     
   }
 
-  getAllProduct() {
+  getAllEvote() {
     this.getEvoteSub = this.evoteService.getAllEvotes().subscribe(
       successResp => {
         this.rows = successResp.content;
@@ -110,5 +112,69 @@ export class EvoteTableComponent implements OnInit {
           );
         }
       });
+  }
+  openProductPopup(data: any = {}, isNew?) {
+    let title = isNew ? "Add new Product" : "Update Product";
+    let dialogRef: MatDialogRef<any> = this.dialog.open(
+      EvotePopupComponent,
+      {
+        width: "720px",
+        disableClose: true,
+        data: { title: title, payload: data }
+      }
+    );
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (!res) {
+        // if user press cancel.
+        return;
+      }
+      this.loader.open();
+
+      console.log("RES obj :");
+      console.log(res);
+
+      res.expireDate = moment(res.expireDate).format("YYYY-MM-DD");
+
+      if (isNew) {
+        this.evoteService.addEvote(res, this.rows).subscribe(
+          data => {
+            this.rows = data;
+            this.loader.close();
+          },
+          error => {
+            this.loader.close();
+            this.errDialog.showError({
+              title: "Error",
+              status: error.status,
+              type: "http_error"
+            });
+          }
+        );
+      } else {
+        this.evoteService.updateEvote(data.id, res).subscribe(
+          response => {
+            console.log(response.content);
+            this.rows = this.rows.map(i => {
+              if (i.id === data.id) {
+                return Object.assign({}, i, response.content);
+              }
+              return i;
+            });
+
+            this.loader.close();
+            return this.rows.slice();
+          },
+          error => {
+            this.loader.close();
+            this.errDialog.showError({
+              title: "Error",
+              status: error.status,
+              type: "http_error"
+            });
+          }
+        );
+      }
+    });
   }
 }
