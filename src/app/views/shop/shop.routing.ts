0099e20a534260445bd1bc1a0e06242b7ba8f5a9
Yasas Ravindu <41327745+YasasRavindu@users.noreply.github.com>
2018-10-19 11:00:45 +0530

    adding restructured future survey component and deleted extra files of shop  module

--- src/app/views/shop/shop.routing.ts
+++ src/app/views/shop/shop.routing.ts
@@ -1,26 +1,16 @@
-import { Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { FutureSurveyComponent } from './future-survey/future-survey.component';
+import { Routes } from "@angular/router";
 
-export const ShopRoutes: Routes = [{
-  path: '',
-  children: [{
-    path: '',
-    component: ProductsComponent
-  }, {
-    path: 'products/:id',
-    component: ProductDetailsComponent,
-    data: { title: 'Detail', breadcrumb: 'Detail' }
-  }, {
-    path: 'cart',
-    component: CartComponent,
-    data: { title: 'Cart', breadcrumb: 'CART' }
-  }, {
-    path: 'sEditor',
-    component: FutureSurveyComponent,
-    data: { title: 'Survey Editor', breadcrumb: 'Survey Editor' }
-  }]
-}]
+import { FutureSurveyComponent } from "./future-survey/future-survey.component";
+
+export const ShopRoutes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "sEditor",
+        component: FutureSurveyComponent,
+        data: { title: "Survey Editor", breadcrumb: "Survey Editor" }
+      }
+    ]
+  }
+];
