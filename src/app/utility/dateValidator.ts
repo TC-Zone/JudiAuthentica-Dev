c2e98821343aa25fe3241660094242b7f87cad0e
Buddhi Hasanka <buddhi.hasanka@gmail.com>
2018-10-29 12:53:38 +0530

    MASTER_bug - Front end & UI related Issues fixing
    # Fix Image Upload issue.
    # Apply it, Add new E vote and Add new Product
    # Validate survey-creation-popup data datepicker.

--- src/app/utility/dateValidator.ts
+++ src/app/utility/dateValidator.ts
@@ -8,11 +8,7 @@ export class DateValidator {
     }
     static getToday() {
         var d = new Date();
-<<<<<<< HEAD
-        var todayDate = d.getDate();
-=======
         var todayDate = d.getDate()
->>>>>>> 69ead0639d8bed2ef9a07c2946a576bc00d98d91
         d.setDate(todayDate);
         return d;
     }
