805ead3382436196ac6df0f4c4eb470923831f8f
Yasas Ravindu <41327745+YasasRavindu@users.noreply.github.com>
2019-01-14 17:14:04 +0530

    Few changes on Invitation setting popup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   apiURL: "productionApi",
 
   // SAP NEW 64GB
-  productApiURL:'https://authentican60aazs63l.ca1.hana.ondemand.com/product/api/',
-  surveyApiURL : 'https://authentican60aazs63l.ca1.hana.ondemand.com/survey/api/',
-  productimageUrl :"https://authentican60aazs63l.ca1.hana.ondemand.com/product/",
-  evoteimageUrl: "https://authentican60aazs63l.ca1.hana.ondemand.com/survey/",
+  // productApiURL:'https://authentican60aazs63l.ca1.hana.ondemand.com/product/api/',
+  // surveyApiURL : 'https://authentican60aazs63l.ca1.hana.ondemand.com/survey/api/',
+  // productimageUrl :"https://authentican60aazs63l.ca1.hana.ondemand.com/product/",
+  // evoteimageUrl: "https://authentican60aazs63l.ca1.hana.ondemand.com/survey/",
 
   // SAP NEW 32GB -  DEV TEST
   // productApiURL:
@@ -17,10 +17,10 @@ export const environment = {
   // evoteimageUrl: "https://authenticak84f365ea.ca1.hana.ondemand.com/survey",
 
   // LOCALE
-  // productApiURL: "http://localhost:10001/api/",
-  // surveyApiURL: "http://localhost:10002/api/",
-  // productimageUrl: "http://localhost:10001/",
-  // evoteimageUrl: "http://localhost:10002/",
+  productApiURL: "http://localhost:10001/api/",
+  surveyApiURL: "http://localhost:10002/api/",
+  productimageUrl: "http://localhost:10001/",
+  evoteimageUrl: "http://localhost:10002/",
 
   // SAP OLD
   // productApiURL: "https://productzg4t4ks63a.hana.ondemand.com/product/api/",
@@ -28,7 +28,7 @@ export const environment = {
   // productimageUrl: "https://productzg4t4ks63a.hana.ondemand.com/product/",
   // evoteimageUrl : 'https://surveyzg4t4ks63a.hana.ondemand.com/survey/'
 
-  // frontEndBaseUrl :'https://judedw.github.io/JudiAuthentica-Dev/',
-  // frontEndBaseUrl: "http://localhost:4200/",
-   frontEndBaseUrl: "https://www.clearpicturevote.com/"
+  //  frontEndBaseUrl :'https://judedw.github.io/JudiAuthentica-Dev/',
+   frontEndBaseUrl: "http://localhost:4200/",
+  // frontEndBaseUrl: "https://www.clearpicturevote.com/"
 };
