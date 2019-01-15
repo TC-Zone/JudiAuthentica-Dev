307f39abc2f06c1426e691c4d0e153cff584ec1c
Yasas Ravindu <41327745+YasasRavindu@users.noreply.github.com>
2019-01-16 03:37:14 +0530

    Invitee group related changes

--- src/app/views/future-survey/future-survey-invitee-group/future-survey-invitee-group.component.ts
+++ src/app/views/future-survey/future-survey-invitee-group/future-survey-invitee-group.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { egretAnimations } from '../../../shared/animations/egret-animations';
+import { Component, OnInit } from "@angular/core";
+import { egretAnimations } from "../../../shared/animations/egret-animations";
+import { FutureSurveyService } from "../future-survey.service";
+import { AppDataConversionService } from "../../../shared/services/data-conversion.service";
+import { AppLoaderService } from "../../../shared/services/app-loader/app-loader.service";
+import { AppErrorService } from "../../../shared/services/app-error/app-error.service";
 
 @Component({
-  selector: 'app-future-survey-invitee-group',
-  templateUrl: './future-survey-invitee-group.component.html',
+  selector: "app-future-survey-invitee-group",
+  templateUrl: "./future-survey-invitee-group.component.html",
   animations: egretAnimations
-
 })
 export class FutureSurveyInviteeGroupComponent implements OnInit {
+  public allInvitations: any[];
 
-  constructor() { }
+  constructor(
+    private futureSurveyService: FutureSurveyService,
+    private conversionService: AppDataConversionService,
+    private errDialog: AppErrorService,
+    private loader: AppLoaderService
+  ) {}
 
   ngOnInit() {
+    this.getAllInvitation();
   }
 
-  testClick(){
+  testClick() {}
 
+  getAllInvitation() {
+    this.futureSurveyService.fetchAllInvitation().subscribe(
+      reponse => {
+        this.allInvitations = reponse.content;
+      },
+      error => {
+        this.errDialog.showError({
+          title: "Error",
+          status: error.status,
+          type: "http_error"
+        });
+      }
+    );
   }
-
 }
