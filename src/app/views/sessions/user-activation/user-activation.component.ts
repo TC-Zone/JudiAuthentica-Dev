169eea40ac88650d0e1d327202d2507b5c72f64d
Buddhi Hasanka <buddhi.hasanka@gmail.com>
2019-02-15 17:38:29 +0530

    User Activation Update

--- src/app/views/sessions/user-activation/user-activation.component.ts
+++ src/app/views/sessions/user-activation/user-activation.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from "@angular/router";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { UserService } from "../UserService.service";
 
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material";
+
 @Component({
   selector: "app-user-activation",
   templateUrl: "./user-activation.component.html"
@@ -10,20 +13,22 @@ import { UserService } from "../UserService.service";
 export class UserActivationComponent implements OnInit {
   activationForm: FormGroup;
   activationCode: string;
-
   errorObj: any;
+  public result = false;
+  public errorMsg = "The password must contain at least 6 characters !";
 
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
-  ) {}
+    private userService: UserService,
+    private snack: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       console.log(params["code"]);
       this.activationCode = params["code"];
     });
-
     this.buildActivationForm();
   }
 
@@ -35,16 +40,30 @@ export class UserActivationComponent implements OnInit {
 
   activateUser() {
     console.log('CALLED activate');
+    if (100 > this.activationForm.value.password.length && this.activationForm.value.password.length > 6) {
+      this.userService
+        .activateUser(this.activationCode, this.activationForm.value)
+        .subscribe(
+          response => {
+            console.log("SUCCESS ACTIVATION");
+            this.result = false;
+            this.snack.open("User Activated !", "OK", { duration: 3000 });
+            this.router.navigate(['sessions/signin']);
+          },
+          error => {
+            if (error.error.validationFailures[0].code === "userActivationRequest.alreadyActivated") {
+              this.errorMsg = "User Already Activated !";
+              this.result = true;
+            }
+          }
+        );
+    } else {
+      this.errorMsg = "The password must contain at least 6 characters !";
+      this.result = true;
+    }
+  }
 
-    this.userService
-      .activateUser(this.activationCode, this.activationForm.value)
-      .subscribe(
-        response => {
-          console.log("SUCCESS ACTIVATION");
-        },
-        error => {
-          this.errorObj = error;
-        }
-      );
+  submit() {
+    console.log('CALLED activate');
   }
 }
