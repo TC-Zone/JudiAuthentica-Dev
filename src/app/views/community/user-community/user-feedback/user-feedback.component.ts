6543048a0b1b6081a7d976ff36d47a64dfe2e26f
Prasad Kumara <contactpkumara@gmail.com>
2019-03-11 18:01:32 +0530

    sample popup view

--- src/app/views/community/user-community/user-feedback/user-feedback.component.ts
+++ src/app/views/community/user-community/user-feedback/user-feedback.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { egretAnimations } from 'app/shared/animations/egret-animations';
+import { AppInfoService } from 'app/shared/services/app-info/app-info.service';
+import { AppWarningService } from 'app/shared/services/app-warning/app-warning.service';
+import { AppErrorService } from 'app/shared/services/app-error/app-error.service';
 
 @Component({
   selector: 'app-user-feedback',
@@ -8,9 +11,46 @@ import { egretAnimations } from 'app/shared/animations/egret-animations';
 })
 export class UserFeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private appWarningService: AppWarningService,
+    private appInfoService: AppInfoService,
+    private errDialog: AppErrorService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  openWarning() {
+    this.appWarningService.showWarning({
+      title: 'Test',
+      message: 'Test Warning!'
+    });
+  }
+
+  openInfo() {
+    const infoData = {
+      title: 'Test',
+      message: 'Test Information',
+      linkData: {
+        url: '#',
+        urlDescription: 'Activate More'
+      }
+    };
+    this.appInfoService.showInfo(infoData);
+  }
+
+  openError() {
+    const error = {
+      error: {
+        status: 'Client_Error',
+        validationFailures: [{
+          code: 'Test Error',
+          field: 'test field'
+        }],
+      },
+      status: 400
+    };
+    this.errDialog.showErrorWithMessage(error);
   }
 
 }
